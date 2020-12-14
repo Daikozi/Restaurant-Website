e27ccfe2bad916c1316cab63f4cb8073e95cf57d
Daikozi <mehdi.amokrane@protonmail.com>
2020-12-14 23:54:16 +0100

    Creating the Home Compoent

---
+++ src/layout/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import home from "../assets/home.png";
+
+const Home = () => {
+  return (
+    <section className="home" id="home">
+      <div className="home__container bd-container bd-grid">
+        <div className="home__data">
+          <h1 className="home__title">Tasty food</h1>
+          <h2 className="home__subtitle">
+            Try the best food of <br /> the week.
+          </h2>
+          <a href="#" className="button">
+            View Menu
+          </a>
+        </div>
+        <img src={home} alt="" className="home__img" />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
